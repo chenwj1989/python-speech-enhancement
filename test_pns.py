f38fde724d576beef24c31cfbdf8704c1540b679
Weijian Chen <weijian.chen@shopee.com>
2024-06-26 17:00:25 +0800

    Add an example of denoising an audio file; example of processing stereo audio

--- test_pns.py
+++ test_pns.py
@@ -5,8 +5,7 @@ from pesq import pesq
 
 from pns.noise_suppressor import NoiseSuppressor
 
-
-def main():
+def test():
 
     # Prepare Data 
     clean_files = ["data/sp02.wav", "data/sp04.wav", "data/sp06.wav", "data/sp09.wav"]
@@ -61,7 +60,54 @@ def main():
             pesq_wb = pesq(ref=clean_wav, deg=xfinal, fs=fs, mode='wb')
             print("output pesq wb: ", "%.4f" % pesq_wb)
 
+
+def denoise_file(input_file, output_file):
+    noisy_wav, fs = sf.read(input_file)
+    channels = noisy_wav.shape[1] if noisy_wav.ndim > 1 else 1
+    print("Input file: ", input_file)
+    print("Sample rate: ", fs, "Hz")
+    print("Num of channels: ", channels)
+    print("Output file: ", output_file)
+
+    if channels > 1 :
+        xfinal = np.zeros(noisy_wav.shape)
+
+        for ch in range(channels):
+            noise_suppressor = NoiseSuppressor(fs)
+            x = noisy_wav
+            frame_size = noise_suppressor.get_frame_size()
+
+            # Start Processing
+            k = 0
+            while k + frame_size < len(x):
+                frame = x[k : k + frame_size, ch]
+                xfinal[k : k + frame_size, ch] =  noise_suppressor.process_frame(frame)
+                k += frame_size
+
+            # Save Results
+            xfinal[:, ch] = xfinal[:, ch] / max(np.abs(xfinal[:, ch]))
+
+    else:
+        # Initialize
+        noise_suppressor = NoiseSuppressor(fs)
+        x = noisy_wav
+        frame_size = noise_suppressor.get_frame_size()
+        xfinal = np.zeros(len(x))
+
+        # Start Processing
+        k = 0
+        while k + frame_size < len(x):
+            frame = x[k : k + frame_size]
+            xfinal[k : k + frame_size] =  noise_suppressor.process_frame(frame)
+            k += frame_size
+
+        # Save Results
+        xfinal = xfinal / max(np.abs(xfinal))
+    
+    sf.write(output_file, xfinal, fs)
+
 if __name__=="__main__":
-    main()
+    denoise_file("data/sp02_train_sn5.wav", "data/sp02_train_sn5_processed.wav")
+    # test()
 
 
